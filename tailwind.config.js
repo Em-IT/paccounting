2708227bdd5108021ca42248e3220882dd521899
EmIT <emad.armoun@gmail.com>
2021-12-21 07:44:42 +0330

    chore: add initial config files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        mypurple : {
+          DEFAULT: "#c600ff",
+          light: "#e073ff",
+          dark: "#79009c"
+        }
+      },
+    },
+  },
+  plugins: [],
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// import path from 'path';
+const path = require("path");
+
+// export default {
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src','frontend'),
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.js','.ts','.jsx','.tsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        open: true,
+        static: { directory: path.join(__dirname) },
+        port: 3000,
+        client: {
+            overlay: true,
+            progress: true,
+        },
+        headers: {
+            'X-Developer': 'Emad Armoun (EmIT)',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?/,
+                exclude: /node_modules/,
+                loader:'babel-loader'
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    }
+}
