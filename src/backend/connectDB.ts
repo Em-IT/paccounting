4628ac9a32fe5204c3ee938d044d085ba30b687a
EmIT <emad.armoun@gmail.com>
2021-12-25 11:42:10 +0330

    feat: add connect db codes

---
+++ src/backend/connectDB.ts
@@ -0,0 +1,17 @@
+import { MongoClient, Db } from 'mongodb';
+
+const dbUrl: string = 'mongodb://localhost:27017/paccounting' || process.env.MONGODB_URI;
+let db: Db;
+
+export const connectDatabase = async () => {
+  if(db)
+    return db;
+
+  const client: MongoClient = new MongoClient(dbUrl);
+           
+  await client.connect();
+       
+  db = client.db();
+
+  return db;
+}
