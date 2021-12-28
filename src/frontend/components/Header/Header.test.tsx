71eb3e175b05108dec86604ec41b4146b2b826d5
EmIT <emad.armoun@gmail.com>
2021-12-28 19:17:04 +0330

    test: use find by attribute helper for test

--- src/frontend/components/Header/Header.test.tsx
+++ src/frontend/components/Header/Header.test.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { Header } from './Header';
+import { findByTestAttr } from '../../helpers/testTools';
 
 const setup = () => shallow(<Header />);
 
-test('Header exists', () => {
-  const wrapper = setup();
-  expect(wrapper.exists()).toBe(true);
+describe('Header Component Tests', () => {
+
+  test('Header exists', () => {
+    const wrapper = setup();
+    expect(wrapper.exists()).toBe(true);
+  });
+  
+  test('Header has a title', () => {
+    const wrapper = setup();
+    const titleNode = findByTestAttr(wrapper, "header-title");
+    expect(titleNode.exists()).toBe(true);
+  });
+
 });
--- src/frontend/components/Header/Header.tsx
+++ src/frontend/components/Header/Header.tsx
@@ -5,7 +5,8 @@ import tw from 'twin.macro';
 export const Header = () => {
   return (
     <div tw="px-10 py-4 bg-indigo-100 text-2xl shadow-md">
-      Header
+      <div data-test="header-title">Header</div>
+      
     </div>
   );
 };
