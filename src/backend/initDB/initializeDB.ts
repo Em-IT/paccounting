f35d1d34b99d43041c9e226b1853077aa088b29f
EmIT <emad.armoun@gmail.com>
2022-01-04 09:42:31 +0330

    feat: add structure of db collections

--- src/backend/initDB/initializeDB.ts
+++ src/backend/initDB/initializeDB.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { defaultState } from "./defaultState";
 import { connectDatabase } from "../connectDB";
@@ -13,6 +14,16 @@ const initializeDB = async () => {
     // No category is available in the database
     if (! anyCategory) {
       logInfo('Initializing Database...');
+
+      db.createCollection('defaultCategories', { validator: { $jsonSchema: { bsonType: 'object', required: ['title',          'isIncome',          'subCategories'], properties: { title: { bsonType: 'string' }, isIncome: { bsonType: 'bool' }, subCategories: { bsonType: 'array', items: {
+        required: ['title'], properties: { title: { bsonType: 'string' } } } } }         }      } });
+      
+      db.createCollection('users', { validator: { $jsonSchema: { bsonType: 'object', required: ['firstName',          'lastName',          'userName',          'password',          'avatar',          'categories'], properties: { firstName: { bsonType: 'string' }, lastName: { bsonType: 'string' }, userName: { bsonType: 'string' }, password: { bsonType: 'string' }, avatar: { bsonType: 'string' }, categories: { bsonType: 'array', items: {
+        required: ['title',          'isIncome',          'subCategories'], properties: { title: { bsonType: 'string' }, isIncome: { bsonType: 'bool' }, subCategories: { bsonType: 'array', items: {
+          required: ['title'], properties: { title: { bsonType: 'string' } } } } } } } }         }      } });
+          
+      db.createCollection(
+        'transactions', { validator: { $jsonSchema: { bsonType: 'object', required: ['title',          'amount',          'isIncome',          'userId',          'date',          'catId',          'subCatId',          'tags',          'isUnexpected'], properties: { title: { bsonType: 'string' }, amount: { bsonType: 'int' }, isIncome: { bsonType: 'bool' }, userId: { bsonType: 'objectId' }, date: { bsonType: 'date' }, catId: { bsonType: 'objectId' }, subCatId: { bsonType: 'objectId' }, tags: { bsonType: 'array', items: { bsonType: 'string' } }, isUnexpected: { bsonType: 'bool' }, description: { bsonType: 'string' } }         }      } });
 
       const collectionNames = Object.keys(defaultState);
       const collectionValues = Object.values(defaultState);
