a678f80fa3e87c5e756385c5f6b5f0abf5703322
EmIT <emad.armoun@gmail.com>
2022-01-15 10:29:43 +0330

    feat: read costs from related api using user id

--- src/frontend/pages/Costs.tsx
+++ src/frontend/pages/Costs.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import apiUrl from '../apiUrl';
 
 const Costs = () => {
+  const [costs, setCosts] = useState([]);
+
+  useEffect(() => {
+    readCosts();
+  }, []);
+
+  const readCosts = async () => {
+    const result = await axios.get(apiUrl + `/user/1/transactions/`, {
+      headers: {
+        'content-type': 'text/json',
+        'userId': '61e08a74927d9e1bc3cfbe79',
+      },
+    });
+    console.log(result.data);
+    setCosts(result.data.data);
+  };
+
   return (
     <div>
-      Costs List
+      <h1>Costs List</h1>
+
+      {
+        costs.map((cost, index) => (
+          <div key={index}>
+            {cost.title}
+            {cost.amount}
+            {cost.date}
+            {cost.primaryCat.title}
+            {cost.secondaryCat.title}
+            {cost.tags}
+            {cost.isUnexpected ? 'True' : 'False'}
+            {cost.description}
+          </div>
+        ))
+      }
     </div>
   );
 };
