5f87dd7e07424efe291b0b10842254a0272dcafb
EmIT <emad.armoun@gmail.com>
2022-01-15 10:30:05 +0330

    refactor: remove sample api test

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -2,21 +2,12 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
-import axios from 'axios';
 
 import GlobalStyles from './GlobalStyles';
 import { store } from './store/index';
-import apiUrl from './apiUrl';
 import Dashboard from './pages/Dashboard';
 import Costs from './pages/Costs';
 import Header from './components/Header';
-
-const readHelloWorld = async () => {
-  const result = await axios.get(apiUrl + `/helloworld/`);
-  console.log(result.data);
-};
-
-readHelloWorld();
 
 ReactDOM.render(
   <StrictMode>
