c78e14dad0b496061ec19b901ddc34ca2d91520d
EmIT <emad.armoun@gmail.com>
2022-01-16 22:43:04 +0330

    feat: use fetch api hook to read costs

--- src/frontend/pages/Costs.tsx
+++ src/frontend/pages/Costs.tsx
@@ -1,42 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import apiUrl from '../apiUrl';
+import React from 'react';
+import useApiCall from '../helpers/apiTools';
 
 const Costs = () => {
-  const [costs, setCosts] = useState([]);
 
-  useEffect(() => {
-    readCosts();
-  }, []);
-
-  const readCosts = async () => {
-    const result = await axios.get(apiUrl + `/my-costs`, {
-      headers: {
-        'content-type': 'text/json',
-        'userId': '61e08a74927d9e1bc3cfbe79',
-      },
-    });
-    console.log(result.data);
-    setCosts(result.data.data);
-  };
-
+  const { data: costs, isLoading, errorMessage } = useApiCall(
+    '/my-costs',
+    { 'userId': '61e08a74927d9e1bc3cfbe79' },
+  );
+  
   return (
     <div>
       <h1>Costs List</h1>
 
+      { isLoading && <span>Is Loading ...</span> }
+
+      { ! isLoading && errorMessage && <span>Error: {errorMessage}</span> }
+
       {
-        costs.map((cost, index) => (
-          <div key={index}>
-            {cost.title}
-            {cost.amount}
-            {cost.date}
-            {cost.primaryCat.title}
-            {cost.secondaryCat.title}
-            {cost.tags}
-            {cost.isUnexpected ? 'True' : 'False'}
-            {cost.description}
-          </div>
-        ))
+        ! isLoading && ! errorMessage && costs && costs.length > 0 &&
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          costs.map((cost: any, index: number) => (
+            <div key={index}>
+              {cost.title}
+              {cost.amount}
+              {cost.date}
+              {cost.primaryCat.title}
+              {cost.secondaryCat.title}
+              {cost.tags}
+              {cost.isUnexpected ? 'True' : 'False'}
+              {cost.description}
+            </div>
+          ))
       }
     </div>
   );
