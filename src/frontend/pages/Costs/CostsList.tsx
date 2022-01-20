300dfd1db76740f8c9f72aa29dd2d14c572efcd9
EmIT <emad.armoun@gmail.com>
2022-01-20 11:10:52 +0330

    feat: use addCommas function for cost amount

--- src/frontend/pages/Costs/CostsList.tsx
+++ src/frontend/pages/Costs/CostsList.tsx
@@ -4,6 +4,7 @@ import tw, { css } from 'twin.macro';
 
 import useApiCall from '../../helpers/apiTools';
 import ICost from '../../types/ICost';
+import { addCommas } from '../../helpers/formatters';
 
 const CostsList = () => {
 
@@ -52,7 +53,7 @@ const CostsList = () => {
                     {cost.title}
                   </td>
                   <td css={[styles.cell]}>
-                    {cost.amount}
+                    {addCommas(cost.amount)}
                   </td>
                   <td css={[styles.cell]}>
                     {cost.date}
