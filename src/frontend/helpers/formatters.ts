a087fb9ab6a4a946065dccee60cb7d078e1f40d8
EmIT <emad.armoun@gmail.com>
2022-01-20 11:10:32 +0330

    feat: add formatter helper file

---
+++ src/frontend/helpers/formatters.ts
@@ -0,0 +1,10 @@
+export const addCommas = (x: number | string) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+export const toFloatingPrecision = (floatNumber: number, precision: number) => {
+  const integerPart: number = Math.floor(floatNumber);
+  const floatingPart: number = floatNumber - integerPart;
+
+  return integerPart + parseFloat(floatingPart.toPrecision(precision));
+};
