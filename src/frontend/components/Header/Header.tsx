eeb4968a15380d337ab790c780ea105e30da879c
EmIT <emad.armoun@gmail.com>
2022-01-27 08:27:17 +0330

    feat: add navigation items

--- src/frontend/components/Header/Header.tsx
+++ src/frontend/components/Header/Header.tsx
@@ -2,15 +2,37 @@ import React from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from 'twin.macro';
 
+import navigationItems, { INavigationItem } from './navigationItems';
+
 interface IHeader {
   title: string;
 }
 
+const styles = {
+  navBar: tw`flex flex-row text-sm text-purple-800`,
+  navItem: tw`flex flex-row items-center`,
+  navItemIcon: tw`w-6 h-6 mx-2`,
+};
+
+// TODO: link nav items to pages
+// TODO: show nav items only in desktop env
 export const Header = ({ title = "Header" } : IHeader) => {
   return (
-    <div tw="px-10 py-4 bg-indigo-100 text-2xl shadow-md">
-      <div data-test="header-title">{title}</div>
+    <div tw="px-10 py-4 bg-indigo-100 shadow-md flex flex-row align-middle">
+      <div data-test="header-title" tw='text-2xl'>{title}</div>
       
+      <ul css={[styles.navBar]}>
+        {
+          navigationItems.map((ni: INavigationItem, index: number) => (
+            <li key={index} css={styles.navItem}>
+              <div css={styles.navItemIcon}>
+                {ni.icon}
+              </div>
+              {ni.title}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 };
