363116e942a8a8f0d3edd8becf23455e902d4a94
EmIT <emad.armoun@gmail.com>
2022-01-28 09:47:54 +0330

    feat: don't show nav bar in mobile env

--- src/frontend/components/Header/Header.tsx
+++ src/frontend/components/Header/Header.tsx
@@ -12,13 +12,12 @@ interface IHeader {
 const styles = {
   headerBar: tw`px-10 py-4 bg-white shadow-md flex flex-row align-middle`,
   appTitle: tw`text-2xl text-purple-800 mr-6`,
-  navBar: tw`flex flex-row items-center text-sm`,
+  navBar: tw`hidden md:flex flex-row items-center text-sm`,
   navItem: tw`flex flex-row items-center text-gray-600 transition-all duration-200
     hover:text-purple-700 hover:font-bold`,
   navItemIcon: tw`w-6 h-6 mx-2`,
 };
 
-// TODO: show nav items only in desktop env
 export const Header = ({ title = "Header" } : IHeader) => {
   return (
     <div css={styles.headerBar}>
