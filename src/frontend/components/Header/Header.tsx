f8ed7ac682fe50bffb34d121227c4b893d07d994
EmIT <emad.armoun@gmail.com>
2022-01-28 09:29:41 +0330

    feat: make nav bar items as link

--- src/frontend/components/Header/Header.tsx
+++ src/frontend/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from 'twin.macro';
+import { Link } from 'react-router-dom';
 
 import navigationItems, { INavigationItem } from './navigationItems';
 
@@ -9,26 +10,31 @@ interface IHeader {
 }
 
 const styles = {
-  navBar: tw`flex flex-row text-sm text-purple-800`,
-  navItem: tw`flex flex-row items-center`,
+  headerBar: tw`px-10 py-4 bg-white shadow-md flex flex-row align-middle`,
+  appTitle: tw`text-2xl text-purple-800 mr-6`,
+  navBar: tw`flex flex-row items-center text-sm`,
+  navItem: tw`flex flex-row items-center text-gray-600 transition-all duration-200
+    hover:text-purple-700 hover:font-bold`,
   navItemIcon: tw`w-6 h-6 mx-2`,
 };
 
-// TODO: link nav items to pages
 // TODO: show nav items only in desktop env
 export const Header = ({ title = "Header" } : IHeader) => {
   return (
-    <div tw="px-10 py-4 bg-indigo-100 shadow-md flex flex-row align-middle">
-      <div data-test="header-title" tw='text-2xl'>{title}</div>
+    <div css={styles.headerBar}>
+      
+      <div data-test="header-title" css={styles.appTitle}>PAccounting</div>
       
       <ul css={[styles.navBar]}>
         {
           navigationItems.map((ni: INavigationItem, index: number) => (
-            <li key={index} css={styles.navItem}>
-              <div css={styles.navItemIcon}>
-                {ni.icon}
-              </div>
-              {ni.title}
+            <li key={index}>
+              <Link to={ni.link} css={styles.navItem}>
+                <div css={styles.navItemIcon}>
+                  {ni.icon}
+                </div>
+                {ni.title}
+              </Link>
             </li>
           ))
         }
