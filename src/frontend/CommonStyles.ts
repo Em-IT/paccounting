60b0f7dd98bc32a1c5d8ede27e3090ae8ddc1e4e
EmIT <emad.armoun@gmail.com>
2022-01-30 09:30:18 +0330

    refactor: separate common styles from global styls

---
+++ src/frontend/CommonStyles.ts
@@ -0,0 +1,10 @@
+import tw from 'twin.macro';
+
+const cStyles = {
+  card: tw`bg-white rounded-lg my-4 p-4`,
+  label: tw`w-24 inline-block text-gray-700`,
+  btn: tw`rounded-md m-2 px-3 py-1`,
+  primaryBtn: tw`bg-purple-700 text-white`,
+};
+
+export default cStyles;
--- src/frontend/GlobalStyles.js
+++ src/frontend/GlobalStyles.js
@@ -2,13 +2,6 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro';
 
-export const gStyles = {
-  card: tw`bg-white rounded-lg my-4 p-4`,
-  label: tw`w-24 inline-block text-gray-700`,
-  btn: tw`rounded-md m-2 px-3 py-1`,
-  primaryBtn: tw`bg-purple-700 text-white`,
-};
-
 const CustomStyles = createGlobalStyle`
   body {
     -webkit-tap-highlight-color: ${theme`colors.purple.500`};
