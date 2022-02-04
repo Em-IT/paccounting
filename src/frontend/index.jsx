6cdc28743156c5a95f739b3feba3fac4bfeef744
EmIT <emad.armoun@gmail.com>
2022-02-04 23:54:53 +0330

    feat: add toast container & css import file

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -2,8 +2,11 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import GlobalStyles from './GlobalStyles';
+import 'react-toastify/dist/ReactToastify.css';
+// import '~react-toastify/dist/ReactToastify.css';
 import { store } from './store/index';
 import Dashboard from './pages/Dashboard';
 import CostsList from './pages/Costs/CostsList';
@@ -12,6 +15,8 @@ import AddCost from './pages/AddCost/AddCost';
 ReactDOM.render(
   <StrictMode>
     <GlobalStyles />
+    <ToastContainer rtl={false} autoClose={3000} />
+
     <Provider store={store}>
       <Router>
         <Switch>
