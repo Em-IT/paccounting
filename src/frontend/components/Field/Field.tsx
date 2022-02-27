1a6783d600d1f7dd8c388a55e0007d9708ea287b
EmIT <emad.armoun@gmail.com>
2022-02-28 00:02:27 +0330

    feat: add checkbox type to Field types

--- src/frontend/components/Field/Field.tsx
+++ src/frontend/components/Field/Field.tsx
@@ -8,23 +8,37 @@ interface FieldProps {
   type: string;
   label: string;
   value: string;
-  setValue: (value: string) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  setValue: (value: any) => void;
 }
 
 export const Field = (props: FieldProps) => {
 
   const { type, label, value, setValue } = props;
-  const VALID_FIELD_TYPES = ["text", "number", "date"];
+  const VALID_FIELD_TYPES = ["text", "number", "date", "checkbox"];
   const isInvalidType = !VALID_FIELD_TYPES.includes(type);
 
   return (
     <div css={cStyles.field}>
       <label css={cStyles.label}>{label}</label>
-      <input type={type} css={cStyles.input}
-        value={isInvalidType ? "Invalid Field Type" : value}
-        onChange={e => setValue(e.target.value)}
-        disabled={isInvalidType}
-      />
+      {
+        type === 'checkbox' && (
+          <div  css={cStyles.input}>
+            <input type="checkbox"
+              checked={value === "true"}
+              onChange={e => setValue(e.target.checked)} />
+          </div>
+        )
+      }
+      {
+        type !== 'checkbox' && (
+          <input type={type} css={cStyles.input}
+            value={isInvalidType ? "Invalid Field Type" : value}
+            onChange={e => setValue(e.target.value)}
+            disabled={isInvalidType}
+          />
+        )
+      }
     </div>
   );
 };
