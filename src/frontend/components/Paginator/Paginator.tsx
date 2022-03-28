741e3f81cf9c7ddf2280098124bbcce9c1c6dde2
EmIT <emad.armoun@gmail.com>
2022-03-28 09:18:38 +0430

    feat: replace paginator button text with icons

--- src/frontend/components/Paginator/Paginator.tsx
+++ src/frontend/components/Paginator/Paginator.tsx
@@ -34,11 +34,37 @@ export const Paginator = (props: PaginatorProps) => {
 
   return (
     <div tw='mx-auto flex justify-center items-center gap-4 my-2'>
-      <button css={[cStyles.btn, cStyles.primaryBtn]} onClick={firstPage}>First Page</button>
-      <button css={[cStyles.btn, cStyles.primaryBtn]} onClick={prevPage}>Prev Page</button>
+
+      <button css={[cStyles.btn, cStyles.primaryBtn]} onClick={firstPage}>
+        <svg xmlns="http://www.w3.org/2000/svg" tw="h-6 w-6" fill="none"
+          viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+        </svg>
+      </button>
+
+      <button css={[cStyles.btn, cStyles.primaryBtn]} onClick={prevPage}>
+        <svg xmlns="http://www.w3.org/2000/svg" tw="h-6 w-6" fill="none"
+          viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+        </svg>
+      </button>
+
       <span>{page} / {Math.ceil(totalCount / 10)}</span>
-      <button css={[cStyles.btn, cStyles.primaryBtn]} onClick={nextPage}>Next Page</button>
-      <button css={[cStyles.btn, cStyles.primaryBtn]} onClick={lastPage}>Last Page</button>
+
+      <button css={[cStyles.btn, cStyles.primaryBtn]} onClick={nextPage}>
+        <svg xmlns="http://www.w3.org/2000/svg" tw="h-6 w-6" fill="none"
+          viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
+      </button>
+
+      <button css={[cStyles.btn, cStyles.primaryBtn]} onClick={lastPage}>
+        <svg xmlns="http://www.w3.org/2000/svg" tw="h-6 w-6" fill="none"
+          viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+        </svg>
+      </button>
+
     </div>
   );
 };
