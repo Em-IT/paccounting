45e81248d4d324f38affdebc65bcf6fb64a8b6c7
EmIT <emad.armoun@gmail.com>
2022-04-05 06:07:09 +0430

    feat: add onclick event to float btn

--- src/frontend/components/FloatAddBtn/FloatAddBtn.tsx
+++ src/frontend/components/FloatAddBtn/FloatAddBtn.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import FloatBtn from '../FloatBtn';
 
 export const FloatAddBtn = () => {
+  const handleClick = () => {
+    console.log('click');
+  };
+
   return (
-    <FloatBtn>
+    <FloatBtn onClick={handleClick}>
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
         viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
--- src/frontend/components/FloatBtn/FloatBtn.tsx
+++ src/frontend/components/FloatBtn/FloatBtn.tsx
@@ -4,12 +4,14 @@ import tw, { css } from 'twin.macro';
 
 interface FloatBtnProps {
   children: JSX.Element;
+  onClick: () => void;
 }
 
 export const FloatBtn = (props: FloatBtnProps) => {
   return (
     <button tw="bg-purple-700 text-white hover:bg-purple-900 transition-all p-4 md:hidden
-      rounded-full fixed right-6 bottom-6 w-16 h-16 flex justify-center items-center">
+      rounded-full fixed right-6 bottom-6 w-16 h-16 flex justify-center items-center"
+    onClick={props.onClick}>
       {props.children}
     </button>
   );
