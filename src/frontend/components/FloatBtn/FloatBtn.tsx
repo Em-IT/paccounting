1c06ad369085ca3cafff4268ac1de77c67c9de88
EmIT <emad.armoun@gmail.com>
2022-04-05 06:02:48 +0430

    feat: hide float btn in desktop mode

--- src/frontend/components/FloatBtn/FloatBtn.tsx
+++ src/frontend/components/FloatBtn/FloatBtn.tsx
@@ -8,7 +8,7 @@ interface FloatBtnProps {
 
 export const FloatBtn = (props: FloatBtnProps) => {
   return (
-    <button tw="bg-purple-700 text-white hover:bg-purple-900 transition-all p-4
+    <button tw="bg-purple-700 text-white hover:bg-purple-900 transition-all p-4 md:hidden
       rounded-full fixed right-6 bottom-6 w-16 h-16 flex justify-center items-center">
       {props.children}
     </button>
