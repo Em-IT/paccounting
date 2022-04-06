9e4867a9d9b5f926101dca9b21c58366c47231f7
EmIT <emad.armoun@gmail.com>
2022-04-06 08:46:49 +0430

    feat: add edit & delete buttons to table

--- src/frontend/pages/Costs/CostTableRow.tsx
+++ src/frontend/pages/Costs/CostTableRow.tsx
@@ -5,6 +5,7 @@ import tw, { css } from 'twin.macro';
 import ICost from '../../types/ICost';
 import { addCommas } from '../../helpers/formatters';
 import { toUKLongDate } from '../../helpers/dateTools';
+import cStyles from '../../CommonStyles';
 
 interface CostTableRowPropType {
   cost: ICost;
@@ -46,6 +47,23 @@ const CostTableRow = ({ cost }: CostTableRowPropType) => {
       </td>
       <td css={[styles.cell, tw`text-gray-700`]}>
         {cost.description}
+      </td>
+      <td css={[styles.cell, tw`text-center`]}>
+        <button css={[cStyles.btn, tw`p-2 m-1`]}>
+          <svg xmlns="http://www.w3.org/2000/svg" tw="h-5 w-5 text-red-800"
+            fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867
+              12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5
+              4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+          </svg>
+        </button>
+        <button css={[cStyles.btn, tw`p-2 m-1`]}>
+          <svg xmlns="http://www.w3.org/2000/svg" tw="h-5 w-5 text-blue-700"
+            fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536
+              3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+          </svg>
+        </button>
       </td>
     </tr>
   );
