dbd9ce4dd53e831f6b52120247c0623dadcd0ade
EmIT <emad.armoun@gmail.com>
2022-04-06 08:47:09 +0430

    style: update btn styles

--- src/frontend/CommonStyles.ts
+++ src/frontend/CommonStyles.ts
@@ -2,8 +2,10 @@ import tw from 'twin.macro';
 
 const cStyles = {
   card: tw`bg-white rounded-lg my-4 p-4`,
-  btn: tw`rounded-md m-2 px-3 py-1`,
-  primaryBtn: tw`bg-purple-700 text-white`,
+  btn: tw`border border-gray-300 rounded-md bg-white cursor-pointer
+    transition-all duration-300 hover:border-gray-500
+    m-2 px-3 py-1`,
+  primaryBtn: tw`bg-purple-700 text-white border-none hover:bg-purple-800`,
 };
 
 export default cStyles;
