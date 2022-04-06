4506c15cf4a19a5958fe242d163072441692d687
EmIT <emad.armoun@gmail.com>
2022-04-06 08:45:37 +0430

    style: add animation to field styles

--- src/frontend/components/Field/Field.styles.ts
+++ src/frontend/components/Field/Field.styles.ts
@@ -3,7 +3,8 @@ import tw from 'twin.macro';
 const fieldStyles = {
   field: tw`flex flex-col my-2`,
   label: tw`text-gray-500 font-bold`,
-  input: tw`p-2 border-2 rounded-md border-gray-100 text-gray-700`,
+  input: tw`p-2 border-2 rounded-md border-gray-100 text-gray-700
+    transition-all duration-300 hover:border-gray-300`,
 };
 
 export default fieldStyles;
